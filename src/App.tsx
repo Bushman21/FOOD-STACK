917b8a0e82936a81589a7b5230d1df74c8e377c4
Heven Hailu <hibbenhailu@gmail.com>
2026-05-18 23:00:41 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,16 @@ const App = () => {
 return (
     <div>
       <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          required
+          placeholder="Enter a search term"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          Recipe Image Location: {recipe.image}
-          <br />
-          Recipe Title: {recipe.title}
-        </div>
-      ))}
+      {/* ... rest of the code */}
     </div>
   );
 };
