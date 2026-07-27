844c68bdfb71661aed8126cc4a4d17d43c1c395f
Heven Hailu <hibbenhailu@gmail.com>
2026-07-27 23:35:11 +0300

    App.tsx Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
 // src/App.tsx
+import React, { FormEvent, useState } from "react";
+import { searchRecipes } from "./API";
+
 const App = () => {
-  return <div>Hey there Iam Bush</div>;
+  
+  const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+return (
+const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const { results } = await searchRecipes(searchTerm, 1);
+      setRecipes(results);
+    } catch (error) {
+      console.error(error);
+);
 };
 
 export default App;
